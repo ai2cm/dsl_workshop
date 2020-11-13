9eaa606a487e03cbe66d07cbc146a071158e72c1
Johann Dahm <johannd@vulcan.com>
2020-11-13 08:43:08 -0800

    Print c++ source if needed

--- notebooks/tools/stencil.py
+++ notebooks/tools/stencil.py
@@ -1,4 +1,12 @@
-def print_stencil_code(stencil):
+import os
+
+def print_stencil_code(stencil, show_cpp=True):
     """Prints the file stencil._file_name to stdout."""
+    backend = stencil._gt_backend_
+    if backend.startswith("gt") and show_cpp:
+        build_dir = os.path.splitext(lap_tmp._file_name)[0] + "_pyext_BUILD"
+        source_file = os.path.join(build_dir, "computation.cpp")
+        with open(source_file, mode="r") as f:
+            print(f.read())
     with open(stencil._file_name, mode="r") as f:
         print(f.read())
