5d783ede4b7195678f1584067904f4c8ce7c6ecc
Oliver Fuhrer <oliverf@vulcan.com>
2020-11-13 10:48:15 -0600

    fix and rename

--- notebooks/tools/stencil.py
+++ notebooks/tools/stencil.py
@@ -4,11 +4,11 @@ from pygments import highlight
 from pygments.lexers import guess_lexer
 from pygments.formatters import TerminalFormatter
 
-def print_stencil_code(stencil, show_cpp=True):
+def print_generated_code(stencil, show_cpp=True):
     """Prints the file stencil._file_name to stdout."""
     backend = stencil._gt_backend_
     if backend.startswith("gt") and show_cpp:
-        build_dir = os.path.splitext(lap_tmp._file_name)[0] + "_pyext_BUILD"
+        build_dir = os.path.splitext(stencil._file_name)[0] + "_pyext_BUILD"
         source_file = os.path.join(build_dir, "computation.cpp")
         with open(source_file, mode="r") as f:
             code = f.read()
